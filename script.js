23fd15526b13d3b5aa03b32014d688cb6bd7116e
Kristen <kristenemayer@gmail.com>
2016-06-05 13:09:19 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+  $("#nav-all").on("click", function(event) {
+    event.preventDefault();
+    $(".flip-card").show();
+  });
+
+  $("#nav-des").on("click", function(event) {
+    event.preventDefault();
+    $(".hide-des").hide();
+    $(".show-des").show();
+  });
+
+  $("#nav-ux").on("click", function(event) {
+    event.preventDefault();
+    $(".hide-ux").hide();
+    $(".show-ux").show();
+  });
+
+  $("#nav-dev").on("click", function(event) {
+    event.preventDefault();
+    $(".hide-dev").hide();
+    $(".show-dev").show();
+  });
+});
