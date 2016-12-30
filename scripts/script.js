c78210d54d4cdd77e9fdd04c53bc1bb34859a26b
Kristen <kristenemayer@gmail.com>
2016-12-30 12:29:23 -0500

    add new cards

--- scripts/script.js
+++ scripts/script.js
@@ -29,43 +29,57 @@ $(document).ready(function () {
     infinite: true
   });
 
-  $( "#sage_grace-card" ).click(function() {
+  $( "#si_622-card" ).click(function() {
+    $( "#si_539-slide" ).slideUp( "slow" );
+    $( "#sage_grace-slide" ).slideUp( "slow" );
     $( "#si_582-slide" ).slideUp( "slow" );
     $( "#si_520-slide" ).slideUp( "slow" );
-    $( "#design_jam_1-slide" ).slideUp( "slow" );
+    $( "#rtf-slide" ).slideUp( "slow" );
+    $( "#si_622-slide" ).slideToggle( "slow" );
+  });
+
+  $( "#si_539-card" ).click(function() {
+    $( "#si_622-slide" ).slideUp( "slow" );
+    $( "#sage_grace-slide" ).slideUp( "slow" );
+    $( "#si_582-slide" ).slideUp( "slow" );
+    $( "#si_520-slide" ).slideUp( "slow" );
+    $( "#rtf-slide" ).slideUp( "slow" );
+    $( "#si_539-slide" ).slideToggle( "slow" );
+  });
+
+  $( "#sage_grace-card" ).click(function() {
+    $( "#si_622-slide" ).slideUp( "slow" );
+    $( "#si_539-slide" ).slideUp( "slow" );
+    $( "#si_582-slide" ).slideUp( "slow" );
+    $( "#si_520-slide" ).slideUp( "slow" );
     $( "#rtf-slide" ).slideUp( "slow" );
     $( "#sage_grace-slide" ).slideToggle( "slow" );
   });
 
   $( "#si_582-card" ).click(function() {
+    $( "#si_622-slide" ).slideUp( "slow" );
+    $( "#si_539-slide" ).slideUp( "slow" );
     $( "#sage_grace-slide" ).slideUp( "slow" );
     $( "#si_520-slide" ).slideUp( "slow" );
-    $( "#design_jam_1-slide" ).slideUp( "slow" );
     $( "#rtf-slide" ).slideUp( "slow" );
     $( "#si_582-slide" ).slideToggle( "slow" );
   });
 
   $( "#si_520-card" ).click(function() {
+    $( "#si_622-slide" ).slideUp( "slow" );
+    $( "#si_539-slide" ).slideUp( "slow" );
     $( "#sage_grace-slide" ).slideUp( "slow" );
     $( "#si_582-slide" ).slideUp( "slow" );
-    $( "#design_jam_1-slide" ).slideUp( "slow" );
     $( "#rtf-slide" ).slideUp( "slow" );
     $( "#si_520-slide" ).slideToggle( "slow" );
   });
 
-  $( "#design_jam_1-card" ).click(function() {
-    $( "#sage_grace-slide" ).slideUp( "slow" );
-    $( "#si_582-slide" ).slideUp( "slow" );
-    $( "#si_520-slide" ).slideUp( "slow" );
-    $( "#rtf-slide" ).slideUp( "slow" );
-    $( "#design_jam_1-slide" ).slideToggle( "slow" );
-  });
-
   $( "#rtf-card" ).click(function() {
+    $( "#si_622-slide" ).slideUp( "slow" );
+    $( "#si_539-slide" ).slideUp( "slow" );
     $( "#sage_grace-slide" ).slideUp( "slow" );
     $( "#si_582-slide" ).slideUp( "slow" );
     $( "#si_520-slide" ).slideUp( "slow" );
-    $( "#design_jam_1-slide" ).slideUp( "slow" );
     $( "#rtf-slide" ).slideToggle( "slow" );
   });
 });
